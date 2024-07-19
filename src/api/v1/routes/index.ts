fe19d24e3bd85d19eaca92583b7f2aef08d7de15
Luis Solano <luisfer.sm15@gmail.com>
2024-07-19 11:28:58 -0600

    fix: removing old swagger configuration

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -1,7 +1,5 @@
-import swaggerUI from 'swagger-ui-express'
 import { Router } from 'express'
 import dotenv from 'dotenv'
-import YAML from 'yamljs'
 import {
   AuthenticationMiddleware,
   AuthorizationMiddleware,
@@ -12,30 +10,10 @@ import security from './security'
 
 dotenv.config()
 
-const swaggerDocumentV1 = YAML.load('./src/api/v1/docs/security.yaml')
-const serverName = process.env.SERVER_URL_NAME ?? ''
 const routes = Router()
 
 /* Middlewares */
 routes.use('/', LanguagesMiddleware)
-
-/* Swagger */
-routes.use(
-  `/docs`,
-  swaggerUI.serve,
-  swaggerUI.setup(undefined, {
-    explorer: true,
-    swaggerOptions: {
-      urls: [
-        {
-          url: `${serverName}/api/v1/swagger/security.yaml`,
-          name: 'Security',
-        },
-      ],
-    },
-  }),
-)
-routes.get('/swagger/security.yaml', (_req, res) => res.json(swaggerDocumentV1))
 
 /* APIs */
 routes.use('/auth', auth)
